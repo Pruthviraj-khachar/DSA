d31e55fb8c2d3abf7be2bc882ddbf8c8e40d1dd0
Pruthviraj-khachar <puthveeraj@gmail.com>
2025-07-15 12:18:48 +0000

    shot 0s,1s,2s array using Dutch National Flag

--- ARRAY/Sort012.C
+++ ARRAY/Sort012.C
@@ -8,7 +8,7 @@ void sort012(int arr[], int n) {
     int low = 0, mid = 0, high = n - 1;
 
     while (mid <= high) {
-        switch (arr[mid]) {
+        switch (arr[mid]) { 
             case 0:
                 // Swap arr[low] and arr[mid]
                 {
